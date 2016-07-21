ba021b818807e6b37d613918db0c137a98aae7c6
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-07-21 17:07:07 +0100

    [feature] create application model

--- bucketlist_application/buppli/models.py
+++ bucketlist_application/buppli/models.py
@@ -2,14 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+from django.db import models
 
-class BucketList(models.Model):
+# Create your models here.
+
+
+class BaseModel(models.Model):
 
     name = models.CharField(max_length=100, blank=True, default='')
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('date_modified',)
+        abstract = True
+
+
+class BucketList(BaseModel):
+
     is_public = models.BooleanField(default=False)
     owner = models.ForeignKey('auth.User', related_name='bucketlists')
 
-    class Meta:
-        ordering = ('date_created',)
+
+class BucketListItem(BaseModel):
+
+    done = models.BooleanField(default=False)
+    bucketlist = models.ForeignKey(BucketList, related_name="items")
