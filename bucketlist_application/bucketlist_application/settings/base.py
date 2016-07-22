44d4d61b2272975ce7724b2a5b7f101a84b58e83
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-07-22 09:35:10 +0100

    [feature] add template settings

--- bucketlist_application/bucketlist_application/settings/base.py
+++ bucketlist_application/bucketlist_application/settings/base.py
@@ -52,6 +52,23 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'bucketlist_application.urls'
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 WSGI_APPLICATION = 'bucketlist_application.wsgi.application'
 
 REST_FRAMEWORK = {
