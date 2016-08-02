f0884b8c222db18c43bb39ad446ff8a39880a8c9
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-02 16:58:52 +0100

    [chore] document helper classes

--- bucketlist_application/buppli/permissions.py
+++ bucketlist_application/buppli/permissions.py
@@ -3,12 +3,14 @@ from buppli.models import BucketList
 
 
 class IsOwner(permissions.BasePermission):
+    """Ensure all allowed access is of the object owner."""
 
     def has_object_permission(self, request, views, obj):
         return obj.owner == request.user
 
 
 class IsBucketListOwner(IsOwner):
+    """Ensure item's bucketlist belong to the requesting user."""
 
     def has_permission(self, request, view):
         bucketlist = BucketList.objects.get(id=view.kwargs['bucketlist_pk'])
@@ -19,10 +21,3 @@ class IsBucketListOwner(IsOwner):
         bucketlist = obj.bucketlist
         return super(IsBucketListOwner,
                      self).has_object_permission(request, views, bucketlist)
-
-
-class AllowCreate(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        if request.method == "POST":
-            return True
