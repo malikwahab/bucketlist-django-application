6a3c4b7818cf7c8d9284c157530116a020485c70
Abdulmalik Abdulwahab <abdulmalik.abdulwahab@andela.com>
2016-08-04 13:02:52 +0100

    [chore] modify settings import

--- bucketlist_application/settings/__init__.py
+++ bucketlist_application/settings/__init__.py
@@ -1,7 +1,9 @@
-import os
 
-if not os.getenv('TRAVIS') and not os.getenv('HEROKU'):
-    from .development import *
+import os
 
 if os.getenv('HEROKU') is not None:
     from .production import *
+elif os.getenv('TRAVIS') is not None:
+    from .test import *
+else:
+    from .development import *
